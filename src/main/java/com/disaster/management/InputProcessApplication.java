cbde6976dc363244e68cc3a87039a91061c2d479
Karan171996 <krnsngh38@gmail.com>
2018-03-24 22:48:26 +0530

    Making Github Repos for inout processing

---
+++ src/main/java/com/disaster/management/InputProcessApplication.java
@@ -0,0 +1,12 @@
+package com.disaster.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InputProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InputProcessApplication.class, args);
+	}
+}
